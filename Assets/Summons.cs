de12bf83aa719a80d588b322f634578377844494
bobjrgeorge <149028752+bobjrgeorge@users.noreply.github.com>
2024-04-14 01:51:47 -0400

    /

--- Assets/Summons.cs
+++ Assets/Summons.cs
@@ -6,12 +6,12 @@ public class Summons : MonoBehaviour
     public List<Transform> AvalibleSummons;
     public Transform pendingAdd;
     private InputSystem input;
-    bool canSummon;
-    public string ID;
+
     //public enemy S_Enemy;
     void Start()
     {
         input = InputSystem.Instance;
+        
     }
 
     // Update is called once per frame
@@ -25,22 +25,21 @@ public class Summons : MonoBehaviour
 
         if(input.PlayerSummonOne())
         {
-            for (int i = 0; i < Object.FindObjectsOfType<Summons>().Length; i++) 
-            {
-                if (Object.FindObjectsOfType<Summons>()[i] != this)
-                {
-                    if (Object.FindObjectsOfType<Summons>()[i].ID == ID) 
-                    {
-                        Destroy(gameObject);
-                    }
-                }
-            }
             if (AvalibleSummons[0] == null)
             {
                 return;
             }
             Instantiate(AvalibleSummons[0], transform.position, Quaternion.identity);
             AvalibleSummons[0].gameObject.SetActive(true);
+        }
+        if(input.PlayerSummonTwo())
+        {
+            if (AvalibleSummons[1] == null)
+            {
+                return;
+            }
+            Instantiate(AvalibleSummons[1], transform.position, Quaternion.identity);
+            AvalibleSummons[1].gameObject.SetActive(true);
         }
     }
 }
