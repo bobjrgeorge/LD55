a8a59828c7901c565c730c66b57747be8b397c40
bobjrgeorge <149028752+bobjrgeorge@users.noreply.github.com>
2024-04-15 09:24:55 -0400

    goop

--- Assets/GoopAttk.cs
+++ Assets/GoopAttk.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using System.Linq;
 
 public class GoopAttk : MonoBehaviour
 {
@@ -11,10 +12,32 @@ public class GoopAttk : MonoBehaviour
     public int goopDamage;
     public float Range;
     public LayerMask Enemy;
+    public List<GameObject> SummonSorter;
+    public string ID;
+    public Summons summons;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        ID = name + summons.AvalibleSummons;
+
+        for (int i = 0; i < Object.FindObjectsOfType<GoopAttk>().Length; i++)
+        {
+            if (Object.FindObjectsOfType<GoopAttk>()[i] != this)
+            {
+                if (Object.FindObjectsOfType<GoopAttk>()[i].ID == ID)
+                {
+                    GameObject summon = FindObjectOfType<GoopAttk>().gameObject;
+                    SummonSorter.Add(summon);
+                    List<GameObject> onlyUniqueObjects = SummonSorter.Distinct().ToList();
+                    for(int j = 0; j < SummonSorter.Count; j++)
+                    {
+                        Destroy(SummonSorter[j]);
+                    }
+                }
+            }
+        }
     }
 
     // Update is called once per frame
@@ -28,17 +51,4 @@ public class GoopAttk : MonoBehaviour
             enemy.GetComponent<enemy>().TakeDamage(goopDamage);
         }
     }
-/*
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("Enemy"))
-        {
-            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, Range, Enemy);
-            foreach (Collider2D enemy in hitEnemies)
-            {
-                enemy.GetComponent<enemy>().TakeDamage(goopDamage);
-            }
-        }
-    }
-*/
 }
